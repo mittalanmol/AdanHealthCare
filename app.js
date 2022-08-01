6bb3d93247eae85e0f905dc23a752e0ce1849f1c
harshvse <harshvse@gmail.com>
2022-08-01 12:31:49 +0530

    updated navbar

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("navbar-expander").addEventListener("click", expandNavbar);
+
+function expandNavbar(){
+    document.getElementsByClassName("menu-text").classList.toggle("hide-show");
+}
