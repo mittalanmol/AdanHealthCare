361cb51314bc385b987b9e33436291643d3c217c
harshvse <harshvse@gmail.com>
2022-08-01 12:51:50 +0530

    anmol Crying

--- app.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("navbar-expander").addEventListener("click", expandNavbar);
-
-function expandNavbar(){
-    document.getElementsByClassName("menu-text").classList.toggle("hide-show");
-}
